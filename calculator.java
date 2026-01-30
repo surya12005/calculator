d9e853ee1b8e4ea5a8f8c3d0d7dbe0e1ea2ce147
surya12005 <245123737136@mvsrec.edu.in>
2026-01-30 15:57:51 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,13 @@
+class calculator
+  {
+    public ststic void main(String args[])
+    {
+      int n1=Integer.parseInt(args[0]);
+      int n2=Integer.parseInt(args[1]);
+      system.out.println("addition:"+n1+n2);
+      system.out.println("substraction:"+n1-n2);
+      system.out.println("division:"+n1/n2);
+      system.out.println("multiplication:"+n1*n2);
+    }
+  }
+      
