3b3e9d8df00f5d9f298e41c325d269ba24e9ccc8
surya12005 <245123737136@mvsrec.edu.in>
2026-02-19 10:08:56 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -4,10 +4,10 @@ class calculator
     {
       int n1=Integer.parseInt(args[0]);
       int n2=Integer.parseInt(args[1]);
-      system.out.println("addition:"+n1+n2);
-      system.out.println("substraction:"+n1-n2);
-      system.out.println("division:"+n1/n2);
-      system.out.println("multiplication:"+n1*n2);
+      System.out.println("addition:"+n1+n2);
+      System.out.println("substraction:"+n1-n2);
+      System.out.println("division:"+n1/n2);
+      System.out.println("multiplication:"+n1*n2);
     }
   }
       
